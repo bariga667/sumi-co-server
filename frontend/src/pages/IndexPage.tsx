441ee4a8dc07ac8aa47117868de42f8a4f6c62f7
Biko <posgos9@gmail.com>
2025-06-12 22:01:36 +0500

    Finel Deploy

--- frontend/src/pages/IndexPage.tsx
+++ frontend/src/pages/IndexPage.tsx
@@ -1,21 +1,6 @@
 import { FC } from "react";
-import { useRecoilValue } from "recoil";
-import { UserRole } from "../types/user";
-import { userRecoilState } from "../recoils/user";
 import { StudentIndexPage } from "./students/StudentIndexPage";
 
 export const IndexPage: FC = () => {
-  const user = useRecoilValue(userRecoilState);
-
-  // Проверка на null перед использованием данных пользователя
-  if (user === null) {
-    // Пока профиль загружается или пользователь не авторизован
-    return <div>Загрузка профиля...</div>;
-  }
-
-  if (user.role === UserRole.Student) {
-    return <StudentIndexPage />;
-  }
-
-  return <div>Если вы видите это, то произошла какая-то ошибка!</div>;
+  return <StudentIndexPage />;
 };
